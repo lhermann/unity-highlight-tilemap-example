2e84c6c1b046a18df43e4459e36515d7ef4ad29d
Lukas Hermann <lukher6@web.de>
2020-10-18 23:47:06 +0200

    Initial Commit

---
+++ Assets/Scripts/GridController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UIElements;
+
+public class GridController : MonoBehaviour
+{
+    private Grid grid;
+    [SerializeField] private Tilemap interactiveMap = null;
+    [SerializeField] private Tilemap pathMap = null;
+    [SerializeField] private Tile hoverTile = null;
+    [SerializeField] private RuleTile pathTile = null;
+
+
+    private Vector3Int previousMousePos = new Vector3Int();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grid = gameObject.GetComponent<Grid>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Mouse over -> highlight tile
+        Vector3Int mousePos = GetMousePos();
+        if (!mousePos.Equals(previousMousePos)) {
+            interactiveMap.SetTile(previousMousePos, null); // Remove old hoverTile
+            interactiveMap.SetTile(mousePos, hoverTile);
+            previousMousePos = mousePos;
+        }
+
+        // Left mouse click -> add path tile
+        if (Input.GetMouseButton(0)) {
+            pathMap.SetTile(mousePos, pathTile);
+        }
+
+        // Right mouse click -> remove path tile
+        if (Input.GetMouseButton(1)) {
+            pathMap.SetTile(mousePos, null);
+        }
+    }
+
+    Vector3Int GetMousePos () {
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        return grid.WorldToCell(mouseWorldPos);
+    }
+}
